0433e336c5850f2416da7e181feae063a978d467
RafoBunyatyan <rafobuniatyan@mail.ru>
2022-09-19 22:48:15 +0400

    add NewQuote hook useHistory

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,8 +1,14 @@
+import { useHistory } from 'react-router-dom';
+
 import QuoteFrom from '../components/quotes/QuoteForm'
 
 const NewQuote = () => {
+	const history = useHistory()
+
 	const addQuoteHandler = (quoteData) => {
 		console.log(quoteData);
+
+		history.push('/quotes')
 	}
 
 	return <QuoteFrom onAddQuote={addQuoteHandler} />
